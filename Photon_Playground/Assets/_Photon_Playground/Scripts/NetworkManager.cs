921139956a816526ef9b6a69baa6c076774cb11b
Iresh Sampath <ireshsampath@gmail.com>
2024-08-17 00:06:45 +0400

    Create new UI flow and loby

--- Photon_Playground/Assets/_Photon_Playground/Scripts/NetworkManager.cs
+++ Photon_Playground/Assets/_Photon_Playground/Scripts/NetworkManager.cs
@@ -2,25 +2,22 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class NetworkManager : MonoBehaviourPunCallbacks
+public class NetworkManager : MonoBehaviourPunCallbacks, ILobbyCallbacks
 {
-    // Instance
-    // public static NetworkManager instance;
+    [SerializeField] UIManager _uIManager;
     [SerializeField] GameObject _agent;
 
-    [SerializeField] TMP_Text _colsolePrint;
+    // Instance
+    // public static NetworkManager instance;
 
-    [SerializeField] TMP_InputField _roomNameInputField;
-    [SerializeField] TMP_InputField _playerNameInputField;
-    [SerializeField] GameObject _createJoinPanel;
+    IEnumerator _coroutine;
 
-    private IEnumerator _coroutine;
+    TypedLobby customLobby = new TypedLobby("customLobby", LobbyType.Default);
+    List<RoomInfo> rooms = new List<RoomInfo>();
 
-    private void Awake()
+    void Awake()
     {
         // If an instance alredy exits and it's not this one - destroy us
         //if (instance != null && instance != this)
@@ -37,73 +34,72 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.AutomaticallySyncScene = true;
     }
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
-
-
-
-    //}
-
-    IEnumerator PrintConsole(string printText)
-    {
-        _colsolePrint.text = printText;
-        yield return new WaitForSeconds(5);
-        _colsolePrint.text = "";
-    }
-
     #region public methods
 
     public void StartMultiplayer()
     {
-        _coroutine = PrintConsole("<color=yellow>Connecting...</color>");
+        _coroutine = _uIManager.PrintConsole("<color=yellow>Connecting...</color>");
         StartCoroutine(_coroutine);
-        _createJoinPanel.SetActive(false);
+
+        _uIManager.CreateJoinPanel.SetActive(false);
 
         //Connect to the Master server
         PhotonNetwork.ConnectUsingSettings();
     }
 
+    public void JoinLobby()
+    {
+        //PhotonNetwork.JoinLobby(customLobby);
+        PhotonNetwork.JoinLobby();
+    }
+
     // Attempts to create a room
     public void CreateRoom()
     {
-        if (_roomNameInputField.text == "")
+        if (_uIManager.RoomNameInputField.text == "")
         {
             StopCoroutine(_coroutine);
-            _coroutine = PrintConsole("<color=red>Room name is required</color>");
+            _coroutine = _uIManager.PrintConsole("<color=red>Room name is required</color>");
             StartCoroutine(_coroutine);
         }
-        else if (_playerNameInputField.text == "")
+        else if (_uIManager.PlayerNameInputField.text == "")
         {
             StopCoroutine(_coroutine);
-            _coroutine = PrintConsole("<color=red>Player name is required</color>");
+            _coroutine = _uIManager.PrintConsole("<color=red>Player name is required</color>");
             StartCoroutine(_coroutine);
         }
         else
         {
-            PhotonNetwork.CreateRoom(_roomNameInputField.text);
+            PhotonNetwork.CreateRoom(_uIManager.RoomNameInputField.text);
         }
     }
 
     // Attempts to join a room
-    public void JoinRoom()
+    public void JoinRoom(GameObject go)
     {
-        if(_roomNameInputField.text == "")
+        if (_uIManager.PlayerNameInputField.text == "")
         {
             StopCoroutine(_coroutine);
-            _coroutine = PrintConsole("<color=red>Room name is required</color>");
-            StartCoroutine(_coroutine);
-        }
-        else if (_playerNameInputField.text == "")
-        {
-            StopCoroutine(_coroutine);
-            _coroutine = PrintConsole("<color=red>Player name is required</color>");
+            _coroutine = _uIManager.PrintConsole("<color=red>Player name is required</color>");
             StartCoroutine(_coroutine);
         }
         else
         {
-            PhotonNetwork.JoinRoom(_roomNameInputField.text);
+            PhotonNetwork.JoinRoom(go.name);
         }
+    }
+
+    public void LeaveServer()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+
+        _uIManager.CreateJoinPanel.SetActive(true);
+        _uIManager.GameHUDPanel.SetActive(false);
     }
 
     // Change the scence using Photon's system
@@ -118,63 +114,70 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     #endregion
 
     #region punCallbacks methods
-
+    
     public override void OnConnectedToMaster()
     {
         StopCoroutine(_coroutine);
-        _coroutine = PrintConsole("<color=green>Connected to the Master Server</color>");
+        _coroutine = _uIManager.PrintConsole("<color=green>Connected to the Master Server</color>");
         StartCoroutine(_coroutine);
 
-        _createJoinPanel.SetActive(true);
+        _uIManager.GameSelectPanel.SetActive(false);
+        _uIManager.CreateJoinPanel.SetActive(true);
+
+        //After we connected to Master server, join the Lobby
+        JoinLobby();
+        //PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        _uIManager.CreateRoomButton(roomList);
     }
 
     public override void OnCreatedRoom()
     {
         StopCoroutine(_coroutine);
-        _coroutine = PrintConsole("<color=green>Created room is: </color>" + PhotonNetwork.CurrentRoom.Name);
+        _coroutine = _uIManager.PrintConsole("<color=green>Created room is: </color>" + PhotonNetwork.CurrentRoom.Name);
         StartCoroutine(_coroutine);
     }
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.NickName = _playerNameInputField.text;
+        PhotonNetwork.NickName = _uIManager.PlayerNameInputField.text;
 
         StopCoroutine(_coroutine);
-        _coroutine = PrintConsole("<color=green>Joined room is: </color>" + PhotonNetwork.CurrentRoom.Name);
+        _coroutine = _uIManager.PrintConsole("<color=green>Joined room is: </color>" + PhotonNetwork.CurrentRoom.Name);
         StartCoroutine(_coroutine);
 
-        _createJoinPanel.SetActive(false);
+        _uIManager.CreateJoinPanel.SetActive(false);
+        _uIManager.GameHUDPanel.SetActive(true);
+
         PhotonNetwork.Instantiate(_agent.name, new Vector3(0, 3, 0), Quaternion.identity);
 
         //LoadScene(1);
     }
 
-    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    public override void OnDisconnected(DisconnectCause cause)
     {
-        foreach (RoomInfo room in roomList)
-        {
-            print(room.Name);
-        }
+        StopCoroutine(_coroutine);
+        _coroutine = _uIManager.PrintConsole("<color=red>" + cause + "</color>");
+        StartCoroutine(_coroutine);
+
+        _uIManager.GameSelectPanel.SetActive(true);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         StopCoroutine(_coroutine);
-        _coroutine = PrintConsole("<color=red>" + message + "</color>");
+        _coroutine = _uIManager.PrintConsole("<color=red>" + message + "</color>");
         StartCoroutine(_coroutine);
 
     }
+
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
         StopCoroutine(_coroutine);
-        _coroutine = PrintConsole("<color=red>" + message + "</color>");
-        StartCoroutine(_coroutine);
-    }
-
-    public override void OnDisconnected(DisconnectCause cause)
-    {
-        StopCoroutine(_coroutine);
-        _coroutine = PrintConsole("<color=red>" + cause + "</color>");
+        _coroutine = _uIManager.PrintConsole("<color=red>" + message + "</color>");
         StartCoroutine(_coroutine);
     }
 
---
+++ Photon_Playground/Assets/_Photon_Playground/Scripts/UIManager.cs
@@ -0,0 +1,78 @@
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject GameSelectPanel;
+    public GameObject CreateJoinPanel;
+    public GameObject GameHUDPanel;
+
+    public TMP_InputField RoomNameInputField;
+    public TMP_InputField PlayerNameInputField;
+
+    [SerializeField] TMP_Text _colsolePrintTxt;
+    [SerializeField] Transform _roomListContainer;
+    [SerializeField] GameObject _roomButtonPrefab;
+    [SerializeField] GameObject _noRoomsTxt;
+
+    List<GameObject> _roomBtns = new List<GameObject>();
+
+    public IEnumerator PrintConsole(string printText)
+    {
+        _colsolePrintTxt.text = printText;
+        yield return new WaitForSeconds(5);
+        _colsolePrintTxt.text = "";
+    }
+
+    public void CreateRoomButton(List<RoomInfo> rooms)
+    {
+        foreach (RoomInfo room in rooms)
+        {
+            bool isNew = true;
+
+            if (room.RemovedFromList)
+            {
+                for (int i = 0; i < _roomBtns.Count(); i++)
+                {
+                    if (room.Name == _roomBtns[i].transform.GetChild(0).name)
+                    {
+                        Destroy(_roomBtns[i]);
+                        _roomBtns.Remove(_roomBtns[i]);
+                    }
+
+                    if(_roomBtns.Count == 0)
+                    {
+                        _noRoomsTxt.SetActive(true);
+                    }
+                }
+            }
+            else
+            {
+                foreach (GameObject btn in _roomBtns)
+                {
+                    if (room.Name == btn.transform.GetChild(0).name)
+                    {
+                        btn.transform.GetChild(0).GetComponent<TMP_Text>().text = room.Name + " " + room.PlayerCount + "/" + room.MaxPlayers;
+                        isNew = false;
+                        break;
+                    }
+                }
+
+                if (isNew)
+                {
+                    GameObject go = Instantiate(_roomButtonPrefab, _roomListContainer);
+                    go.SetActive(true);
+                    go.transform.GetChild(0).GetComponent<TMP_Text>().text = room.Name + " " + room.PlayerCount + "/" + room.MaxPlayers;
+                    go.transform.GetChild(0).name = room.Name;
+
+                    _roomBtns.Add(go);
+                    _noRoomsTxt.SetActive(false);
+                }
+            }
+        }
+    }
+}
